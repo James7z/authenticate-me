258b4a2ab3a13086427034919588374f707eb271
James Zhou <james7.ca@gmail.com>
2023-01-25 12:15:14 -0500

    Get details of a Spot from an id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -5,6 +5,20 @@ const router = express.Router();
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
+
+//spot id check
+const spotIdCheck = async (req, res, next) => {
+    const spot = await Spot.findByPk(req.params.spotId);
+    if (!spot) {
+
+        res.status(404).json({
+            "message": "Spot couldn't be found",
+            "statusCode": 404
+        })
+    }
+    next();
+}
+
 // Get all Spots
 
 router.get('/',
@@ -68,12 +82,9 @@ router.get('/current',
                 ownerId: user.id
             }
         })
-
         let spotsList = [];
-
         spots.forEach(spot => spotsList.push(spot.toJSON()));
         //console.log(spotsList)
-
         spotsList.forEach(spot => {
             // avgStarRating
             if (spot.Reviews.length > 0) {
@@ -92,7 +103,43 @@ router.get('/current',
             delete (spot.SpotImages);
         })
         return res.json(spotsList);
-    }
-)
+    })
+
+//Get details of a Spot from an id
+router.get('/:spotId', spotIdCheck, async (req, res) => {
+    let spot = await Spot.findByPk(req.params.spotId, {
+        include: [{
+            model: Review,
+            attributes: ["stars"]
+        },
+        {
+            model: SpotImage,
+            attributes: ["id", "url", "preview"]
+        },
+        {
+            model: User,
+            attributes: ['id', 'firstName', 'lastName']
+        }
+        ],
+    })
+    spot = spot.toJSON();
+    //console.log(spot)
+    // avgStarRating
+    spot.numReviews = spot.Reviews.length
+    if (spot.Reviews.length > 0) {
+        let total = spot.Reviews.reduce((sum, review) => {
+            return sum += +review.stars;
+        }, 0);
+        //console.log(total)
+        spot.avgStarRating = total / spot.Reviews.length;
+    } else spot.avgStarRating = 'No Review for this spot';
+
+    delete (spot.Reviews);
+    spot.Owner = spot.User;
+    delete spot.User;
+
+    return res.json(spot);
+})
+
 
 module.exports = router;
