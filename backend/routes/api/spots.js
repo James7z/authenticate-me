945426aea55726bc03dd81a5c72ee608dcbd74ea
James Zhou <james7.ca@gmail.com>
2023-01-25 10:53:26 -0500

    Get all Spots by Current User

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -12,33 +12,86 @@ router.get('/',
         const spots = await Spot.findAll({
             include: [{
                 model: Review,
-                attributes: []
+                attributes: ["stars"]
             },
             {
                 model: SpotImage,
-                attributes: ["url"],
-                where: {
-                    preview: true
-                }
+                attributes: ["url", "preview"],
+
             }
             ],
-            // attributes: {
-            //     include: [Sequelize.fn("AVG", Sequelize.col(Review.stars), "avgRating")]
-            // }
-
-
-
         })
-        return res.json(spots);
+
+        let spotsList = [];
+
+        spots.forEach(spot => spotsList.push(spot.toJSON()));
+        //console.log(spotsList)
+
+        spotsList.forEach(spot => {
+            // avgStarRating
+            if (spot.Reviews.length > 0) {
+                let total = spot.Reviews.reduce((sum, review) => {
+                    //console.log(typeof review.stars)
+                    return sum += +review.stars;
+                }, 0);
+                console.log(total)
+                spot.avgStarRating = total / spot.Reviews.length;
+            } else spot.avgStarRating = 'No Review for this spot';
+
+            // "previewImage"
+            spot.SpotImages.forEach(image => {
+                if (image.preview === true) spot.previewImage = image.url;
+            })
+            if (!spot.previewImage) spot.previewImage = "No preview image for this spot"
+
+            delete (spot.Reviews);
+            delete (spot.SpotImages);
+        })
+        return res.json(spotsList);
     })
 
-
+//Get all Spots owned by the Current User
 router.get('/current',
+    requireAuth,
     async (req, res) => {
         const { user } = req;
+        const spots = await Spot.findAll({
+            include: [{
+                model: Review,
+                attributes: ["stars"]
+            },
+            {
+                model: SpotImage,
+                attributes: ["url", "preview"]
+            }],
+            where: {
+                ownerId: user.id
+            }
+        })
 
+        let spotsList = [];
 
-        return res.json(user)
+        spots.forEach(spot => spotsList.push(spot.toJSON()));
+        //console.log(spotsList)
+
+        spotsList.forEach(spot => {
+            // avgStarRating
+            if (spot.Reviews.length > 0) {
+                let total = spot.Reviews.reduce((sum, review) => {
+                    return sum += +review.stars;
+                }, 0);
+                console.log(total)
+                spot.avgStarRating = total / spot.Reviews.length;
+            } else spot.avgStarRating = 'No Review for this spot';
+            // "previewImage"
+            spot.SpotImages.forEach(image => {
+                if (image.preview === true) spot.previewImage = image.url;
+            })
+            if (!spot.previewImage) spot.previewImage = "No preview image for this spot"
+            delete (spot.Reviews);
+            delete (spot.SpotImages);
+        })
+        return res.json(spotsList);
     }
 )
 
